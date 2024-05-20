6a81366e18577d7e8d2fabef8e870a3964c92906
Nunez Dorian <doriannunez64@gmail.com>
2024-05-20 22:06:58 +0200

    PUT AND POST

--- server/index.js
+++ server/index.js
@@ -44,7 +44,84 @@ app.get("/api", (req, res) => {
     });
 });
 
+app.put('/pings/:id', (req, res) => {
+    const pingId = req.params.id;
+    const { status } = req.body;
 
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: '3il_map_db'
+    });
+
+    console.log('Connexion à la base de données...');
+    connection.connect((err) => {
+        if (err) {
+            console.error('Erreur de connexion à la base de données :', err);
+            res.status(500).json({ error: 'Erreur de connexion à la base de données' });
+            return;
+        }
+
+        connection.query('UPDATE pings SET status = ? WHERE id = ?', [status, pingId], (err, result) => {
+            if (err) {
+                console.error('Erreur lors de l\'exécution de la requête :', err);
+                res.status(500).json({ error: 'Erreur lors de l\'exécution de la requête' });
+                return;
+            }
+
+            res.json({ message: 'Ping updated successfully' });
+
+            // Maintenant que toutes les requêtes ont été exécutées, nous pouvons fermer la connexion.
+            connection.end((err) => {
+                if (err) {
+                    console.error('Erreur lors de la fermeture de la connexion à la base de données :', err);
+                    return;
+                }
+                console.log('Connexion à la base de données fermée');
+            });
+        });
+    });
+});
+
+app.post('/pings/create', (req, res) => {
+    const { alias, position, nom, description, is_accessible, indice_cout_vie, comparaison, distance, passeport, langue, timezone, automne_semestre, lien_ecole } = req.body;
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: '3il_map_db'
+    });
+
+    console.log('Connexion à la base de données...');
+    connection.connect((err) => {
+        if (err) {
+            console.error('Erreur de connexion à la base de données :', err);
+            res.status(500).json({ error: 'Erreur de connexion à la base de données' });
+            return;
+        }
+
+        connection.query('INSERT INTO pings (alias, position, nom, description, is_accessible, indice_cout_vie, comparaison, distance, passeport, langue, timezone, automne_semestre, lien_ecole) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)  WHERE id = ?', [alias, position, nom, description, is_accessible, indice_cout_vie, comparaison, distance, passeport, langue, timezone, automne_semestre, lien_ecole, req.params.id], (err, result) => {
+            if (err) {
+                console.error('Erreur lors de l\'exécution de la requête :', err);
+                res.status(500).json({ error: 'Erreur lors de l\'exécution de la requête' });
+                return;
+            }
+
+            res.json({ message: 'Ping created successfully' });
+
+            // Maintenant que toutes les requêtes ont été exécutées, nous pouvons fermer la connexion.
+            connection.end((err) => {
+                if (err) {
+                    console.error('Erreur lors de la fermeture de la connexion à la base de données :', err);
+                    return;
+                }
+                console.log('Connexion à la base de données fermée');
+            });
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
